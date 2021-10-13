e3172773d50193f764b6b57c1e5990e0223352b7
kyuwon53 <lkw2v@naver.com>
2021-10-13 23:21:24 +0900

    [Fix] let 변하는 타입

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const a = 10;
 const b = 2;
-const myName = "kyuwon";
+let myName = "kyuwon";
 
 console.log(a + b);
 console.log(a * b);
 console.log(a / b);
 console.log("hello " + myName);
+
+myName = "kyu";

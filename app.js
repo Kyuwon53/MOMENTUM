00baae12bdbd92301f8958dfa96df8fe56ae71f5
kyuwon53 <lkw2v@naver.com>
2021-10-13 23:37:32 +0900

    [Fix] boolean , true, false, null, undefiend 추가

--- app.js
+++ app.js
@@ -10,3 +10,11 @@ console.log("hello " + myName);
 myName = "kyu";
 
 console.log("your new name is " + myName);
+
+// boolean
+// true 켜짐, false 꺼짐
+// null 비어있음
+// undefiend variavble 이 메모리에 만들어졌지만 값이 없는 것 
+const amIFat = null;
+let something;
+console.log(something); // undefined

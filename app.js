f899f572e3403bfd466996f8ac000c7fd1006eab
kyuwon53 <lkw2v@naver.com>
2021-10-15 00:42:46 +0900

    [Add] Objects

--- app.js
+++ app.js
@@ -29,3 +29,39 @@ console.log(daysOfWeek);
 
 // Add one more day to the array
 daysOfWeek.push("sun");
+
+console.log(daysOfWeek); 
+
+const apple = "apple";
+const potato = "potato"
+const toBuy = ["potato", "tomato", "pizza"];
+toBuy.push("kimbab");
+
+// Obects
+const playerName = "kyuwon";
+const playerPoints = 535353;
+const playerHansome = true;
+const playerFat = "little bit";
+
+// player[0] == name ,...
+// const player = ["kyuwon",535353,true, "little bit" ];
+
+const player = {
+  name: "kyuwon",
+  points: 535353,
+  hansome: true,
+  fat: "little bit"
+};
+
+console.log(player);
+console.log(player.name);
+console.log(player["name"]);
+
+// data update
+console.log(player);
+player.fat = "no";
+console.log(player);
+
+// data add
+player.lastName = "lee";
+console.log(player);

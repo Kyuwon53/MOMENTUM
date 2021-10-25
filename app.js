b91a6a391bfcb55f6fe108e35490ef53ee81082e
kyuwon53 <lkw2v@naver.com>
2021-10-26 00:03:56 +0900

    [Add] localStorage 사용해서 username 저장

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = loginForm.querySelector("#login-form input");
-const link = loginForm.querySelector("a");
 const greeting = document.querySelector("#greeting");
+
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
 function onLoginBtnClick() {
     const username = loginInput.value;
@@ -14,18 +15,23 @@ function onLoginSubmit(event) {
     event.preventDefault();
     loginForm.classList.add(HIDDEN_CLASSNAME);
     const username = loginInput.value;
-    greeting.innerText = '';
-    // greeting.innerText = "Hello! " + username;
+    localStorage.setItem(USERNAME_KEY, username);
+    paintGreetings(username)
+}
+
+function paintGreetings(username){
     greeting.innerText = `Hello! ${username}`;
-    greeting.classList.remove(HIDDEN_CLASSNAME)
-    console.log(username);
+    greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-function handlerLinkClick(event){
-    event.preventDefault();
-    console.log(event);
-    alert("clicked!!!!");
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if(savedUsername === null){
+    // show the form
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+    // show the greetings
+    paintGreetings(savedUsername);
 }
 
-loginForm.addEventListener("submit", onLoginSubmit);
-link.addEventListener("click", handlerLinkClick);

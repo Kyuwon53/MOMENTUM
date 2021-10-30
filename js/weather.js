0df88c860dc93d962f257988514f583c3c82c5c1
kyuwon53 <lkw2v@naver.com>
2021-10-30 23:59:42 +0900

    [Add] 지역 날씨 추가

--- js/weather.js
+++ js/weather.js
@@ -1,7 +1,18 @@
+const API_KEY = "604cdb5234582ce65e1555fd5df35291";
+
 function onGeoOk(position){
   const lat = position.coords.latitude;
-  const lng = position.coords.longitude;
-  console.log("You live it", lat, lng);
+  const lon = position.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
+  console.log(url);
+  fetch(url)
+  .then((response) => response.json())
+  .then((data) => {
+      const weather = document.querySelector("#weather span:first-child");
+      const city = document.querySelector("#weather span:last-child");
+      city.innerText = data.name;
+      weather.innerText =  `${data.weather[0].main} / ${data.main.temp}`;
+    });
 }
 function onGeoError() {
   alert("Can't find you. No weather for you.");
